f877b5c81fa8429dfe55a53a481f25b8058238d2
momen-mohamed <momenmohamed64@gmail.com>
2023-03-08 02:33:02 +0200

    added Solve Me First

---
+++ submissions/Solve Me First.cpp
@@ -0,0 +1,30 @@
+/*-----------------------------------------------------------------------
+
+Problem Title: Solve Me First
+Problem Link: https://www.hackerrank.com/challenges/solve-me-first
+Author: momen4ever19@yahoo.com
+Language : C++
+
+-----------------------------------------------------------------------*/
+
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int solveMeFirst(int a, int b)
+{
+    return a + b;
+}
+
+int main()
+{
+    int num1, num2;
+    int sum;
+    cin >> num1 >> num2;
+
+    return 0;
+}
